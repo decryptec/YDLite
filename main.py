3973f0e0efe2dc1b39f3fb8bc771e7b2cf17f35d
Don Huynh <125177998+decryptec@users.noreply.github.com>
2025-11-07 21:22:03 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import json
 import yt_dlp
 import flask
+
+URL = input("Insert URL: ")
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+  info = ydl.extract_info(URL, downdload=False)
+
+  print(json.dumps(ydl.sanitize_info(info)))
