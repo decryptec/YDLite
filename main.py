e46113ae5228eb288fe5357bacec568d3920f221
Don Huynh <125177998+decryptec@users.noreply.github.com>
2025-11-08 21:44:39 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 import yt_dlp
-import flask 
+from flask import Flask 
 
 # Defined Options for yt_dlp
 ydl_opts = {
@@ -8,11 +8,14 @@ ydl_opts = {
     'outtmpl': '%(title)s.%(ext)s'
 }
 
-URL = input("Insert URL: ")
-
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    info = ydl.extract_info(URL, download=False)
-    print(json.dumps(ydl.sanitize_info(info), indent=2))
-
-    error_code = ydl.download([URL])
-    print('Failed to download' if error_code else 'Successfully downloaded')
+app = Flask(__name__)
+@app.route("/")
+def url_download():
+    URL = input("Insert URL: ")
+    
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(URL, download=False)
+        print(json.dumps(ydl.sanitize_info(info), indent=2))
+    
+        error_code = ydl.download([URL])
+        print('Failed to download' if error_code else 'Successfully downloaded')
