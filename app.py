774b515f43e4e8267f83a53e0e898f00f7f3a176
Don <125177998+decryptec@users.noreply.github.com>
2025-11-30 09:09:06 -0800

    Utilize threaded flask for multiple requests

--- app.py
+++ app.py
@@ -190,4 +190,4 @@ def index():
     return render_template_string(html, result=result)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000, threaded=True)
